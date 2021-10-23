0ea313aa271e5c032b01a5075196e7bcd3e21a8b
Nazar Pechevystyi <nazarpechka@gmail.com>
2021-10-23 18:44:58 +0200

    Initial Commit

---
+++ WroclawTravelGuide/WroclawTravelGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  WroclawTravelGuideApp.swift
+//  WroclawTravelGuide
+//
+//  Created by Nazar Pechevystyi on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct WroclawTravelGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
