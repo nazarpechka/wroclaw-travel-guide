437bd007a8ab59867f79b84f07153da6fa7ff92f
Nazar Pechevystyi <nazarpechka@gmail.com>
2021-10-24 12:37:43 +0200

    Adds two more buttons, makes width more adaptive

--- WroclawTravelGuide/CategoryButton.swift
+++ WroclawTravelGuide/CategoryButton.swift
@@ -11,7 +11,6 @@ struct CategoryButton: View {
     var image: Image
     var label: String
     var backgroundColor: Color
-    var shadowColor: Color
     
     var body: some View {
         Button {
@@ -19,21 +18,24 @@ struct CategoryButton: View {
         } label: {
             VStack {
                 image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80, alignment: .center)
                 Text(label)
             }
+            .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 143)
+            .background(backgroundColor)
+            .foregroundColor(.black)
+            .font(.headline)
+            .cornerRadius(15)
+            .shadow(color: Color("ShadowColor"), radius: 5, x: 0, y: 10)
             
         }
-        .frame(width: 162, height: 143)
-        .background(backgroundColor)
-        .foregroundColor(.black)
-        .font(.headline)
-        .cornerRadius(15)
-        .shadow(color: shadowColor, radius: 5, x: 0, y: 10)
     }
 }
 
 struct CategoryButton_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryButton(image: Image("placeslogo"), label: "Places to visit", backgroundColor: Color("PlacesColor"), shadowColor: Color("PlacesShadowColor"))
+        CategoryButton(image: Image("placeslogo"), label: "Places to visit", backgroundColor: Color("PlacesColor"))
     }
 }
--- WroclawTravelGuide/HomePageView.swift
+++ WroclawTravelGuide/HomePageView.swift
@@ -9,19 +9,29 @@ import SwiftUI
 
 struct HomePageView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("Wroclaw Travel Guide")
                 .font(.largeTitle)
             
-            HStack() {
-                CategoryButton(image: Image("placeslogo"), label: "Places to visit", backgroundColor: Color("PlacesColor"), shadowColor: Color("PlacesShadowColor"))
+            HStack(spacing: 20) {
+                CategoryButton(image: Image("placeslogo"), label: "Places to visit", backgroundColor: Color("PlacesColor"))
                 
-                CategoryButton(image: Image("yourstaylogo"), label: "Your stay", backgroundColor: Color("YourStayColor"), shadowColor: Color("PlacesShadowColor"))
+                CategoryButton(image: Image("yourstaylogo"), label: "Your stay", backgroundColor: Color("YourStayColor"))
                     
             }
             
+            HStack(spacing: 20) {
+                CategoryButton(image: Image("foodlogo"), label: "Food", backgroundColor: Color("FoodColor"))
+                
+                CategoryButton(image: Image("shoppinglogo"), label: "Shopping", backgroundColor: Color("ShoppingColor"))
+                
+            }
+            
+            
             Spacer()
         }
+        .frame(width: UIScreen.main.bounds.width-30,
+               alignment: .center)
         .padding()
     }
 }
