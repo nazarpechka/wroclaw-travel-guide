55d80fe53945961bdd815ac534abf3826231700b
Nazar Pechevystyi <nazarpechka@gmail.com>
2021-10-24 13:16:45 +0200

    Makes buttons on home screen navigation links

--- WroclawTravelGuide/CategoryButton.swift
+++ WroclawTravelGuide/CategoryButton.swift
@@ -11,26 +11,30 @@ struct CategoryButton: View {
     var image: Image
     var label: String
     var backgroundColor: Color
+    @State var selection: Int? = nil
     
     var body: some View {
-        Button {
-            
-        } label: {
-            VStack {
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80, alignment: .center)
-                Text(label)
+        NavigationLink(destination: Text("Test"), tag: 1, selection: $selection) {
+            Button {
+                self.selection = 1
+            } label: {
+                VStack {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80, alignment: .center)
+                    Text(label)
+                }
+                .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 143)
+                .background(backgroundColor)
+                .foregroundColor(.black)
+                .font(.headline)
+                .cornerRadius(15)
+                .shadow(color: Color("ShadowColor"), radius: 5, x: 5, y: 10)
+                
             }
-            .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 143)
-            .background(backgroundColor)
-            .foregroundColor(.black)
-            .font(.headline)
-            .cornerRadius(15)
-            .shadow(color: Color("ShadowColor"), radius: 5, x: 0, y: 10)
-            
         }
+        
     }
 }
 
--- WroclawTravelGuide/HomePageView.swift
+++ WroclawTravelGuide/HomePageView.swift
@@ -8,31 +8,55 @@
 import SwiftUI
 
 struct HomePageView: View {
+    
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Wroclaw Travel Guide")
-                .font(.largeTitle)
-            
-            HStack(spacing: 20) {
-                CategoryButton(image: Image("placeslogo"), label: "Places to visit", backgroundColor: Color("PlacesColor"))
+        
+        NavigationView {
+            VStack(spacing: 20) {
                 
-                CategoryButton(image: Image("yourstaylogo"), label: "Your stay", backgroundColor: Color("YourStayColor"))
+                Text("Wroclaw Travel Guide")
+                    .font(.largeTitle)
+                
+                
+                VStack {
+                    HStack(spacing: 20) {
+                        CategoryButton(image: Image("placeslogo"), label: "Places to visit", backgroundColor: Color("PlacesColor"))
+                        
+                        CategoryButton(image: Image("yourstaylogo"), label: "Your stay", backgroundColor: Color("YourStayColor"))
+                        
+                    }
                     
-            }
-            
-            HStack(spacing: 20) {
-                CategoryButton(image: Image("foodlogo"), label: "Food", backgroundColor: Color("FoodColor"))
+                    HStack(spacing: 20) {
+                        CategoryButton(image: Image("foodlogo"), label: "Food", backgroundColor: Color("FoodColor"))
+                        
+                        CategoryButton(image: Image("shoppinglogo"), label: "Shopping", backgroundColor: Color("ShoppingColor"))
+                        
+                    }
+                    
+                }
                 
-                CategoryButton(image: Image("shoppinglogo"), label: "Shopping", backgroundColor: Color("ShoppingColor"))
                 
+                
+                HStack {
+                    Image("cloudy")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 45, height: 45, alignment: .center)
+                    Text("Cloudy 15°")
+                }
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                
+                
+                Spacer()
             }
-            
-            
-            Spacer()
+            .frame(width: UIScreen.main.bounds.width-30,
+                   alignment: .center)
+            .padding()
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
         }
-        .frame(width: UIScreen.main.bounds.width-30,
-               alignment: .center)
-        .padding()
     }
 }
 
